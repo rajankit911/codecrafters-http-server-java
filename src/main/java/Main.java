bb47e5efd59c247cad8292a0f5d0ff414f6f9203
rajankit911 <rajankit420@gmail.com>
2024-12-02 19:09:18 +0530

    extract url path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(4221);
+        try (ServerSocket serverSocket = new ServerSocket(4221)) {
 
             // Since the tester restarts your program quite often, setting SO_REUSEADDR
             // ensures that we don't run into 'Address already in use' errors
@@ -14,7 +15,28 @@ public class Main {
 
             Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
             System.out.println("accepted new connection");
-            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes(StandardCharsets.UTF_8));
+            String response;
+            try {
+                InputStream is = clientSocket.getInputStream();
+                BufferedInputStream bis = new BufferedInputStream(is);
+                byte[] buffer = new byte[1024];
+                int len = bis.read(buffer);
+                String receivedData = new String(buffer, 0, len);
+                System.out.println("Received: " + receivedData);
+                String[] parts = receivedData.split("\r\n");
+                String[] statusLine = parts[0].split(" ");
+                System.out.println("URL: " + statusLine[1]);
+                if ("/".equals(statusLine[1])) {
+                    response = "HTTP/1.1 200 OK\r\n\r\n";
+                } else {
+                    response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            clientSocket.getOutputStream().write(response.getBytes(StandardCharsets.UTF_8));
+            System.out.println("connection closed!");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
